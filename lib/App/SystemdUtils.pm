1b441fc3cd3ab32c6fcb686c5d4094fd9cc07d7d
perlancar (on pc-office) <perlancar@gmail.com>
2019-05-22 14:02:07 +0700

    Initial commit

---
+++ lib/App/SystemdUtils.pm
@@ -0,0 +1,20 @@
+package App::SystemdUtils;
+
+# DATE
+# VERSION
+
+1;
+#ABSTRACT: CLI utilities related to Systemd
+
+=head1 DESCRIPTION
+
+This distributions provides the following command-line utilities:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<Systemd::Util>
+
+=cut
